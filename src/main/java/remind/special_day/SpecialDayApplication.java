b57372ab605b2b53f837d5f55208b1aa0307bfb6
korimse <korimse@gmail.com>
2021-07-13 20:19:43 +0900

    Initial commit

---
+++ src/main/java/remind/special_day/SpecialDayApplication.java
@@ -0,0 +1,13 @@
+package remind.special_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/remind/special_day/SpecialDayApplicationTests.java
@@ -0,0 +1,13 @@
+package remind.special_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
